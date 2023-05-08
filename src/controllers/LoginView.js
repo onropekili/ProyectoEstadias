5f37b71bd4a6cf5755b169fece310f9c6d82b6b6
Yahir <cristopher.alvarado.21s@utzmg.edu.mx>
2023-05-08 16:45:04 -0600

    Refactor login controller

--- src/controllers/LoginView.js
+++ src/controllers/LoginView.js
@@ -1,47 +1,44 @@
 const databaseInstance = require("../db");
 
-
-
 const loginView = (req, res) => {
   res.send("This is login view");
 };
 
- const Loginlogic = async (req, res) => {
-  const {username, password} = req.body;
+const Loginlogic = async (req, res) => {
+  const { username, password } = req.body;
 
   console.log(req.body);
 
   const user = await databaseInstance.query(
-    "SELECT * from usuario where username = $1 and password = $2", [username, password]
+    "SELECT * from usuario where username = $1 and password = $2",
+    [username, password]
   );
 
- const whereToRedirectUser = verifyIfUserExists(user);
+  const whereToRedirectUser = verifyIfUserExists(user);
 
- res.send(whereToRedirectUser);
-  
+  res.send(whereToRedirectUser);
 };
-
 
 function verifyIfUserExists(user) {
   const hasNumberOfUserRows = user.rows.length;
   const numberOfRowsUserExists = 1;
 
-  if(hasNumberOfUserRows === numberOfRowsUserExists){
+  if (hasNumberOfUserRows === numberOfRowsUserExists) {
     return userExists();
-  }else{
+  } else {
     return userDoesntExist();
   }
 }
 
-function userExists(){
-  return 'This user Exists';
+function userExists() {
+  return "This user Exists";
 }
 
-function userDoesntExist(){
-  return 'This user doesnt exist';
+function userDoesntExist() {
+  return "This user doesnt exist";
 }
 
 module.exports = {
   loginView,
-  Loginlogic
+  Loginlogic,
 };
