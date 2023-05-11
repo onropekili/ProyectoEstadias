0f5c9f7d9ca4909fb84008094caa098b9eb2077f
Yahir <cristopher.alvarado.21s@utzmg.edu.mx>
2023-05-11 15:10:45 -0600

    Fix shop controller

--- src/controllers/ShopController.js
+++ src/controllers/ShopController.js
@@ -1,8 +1,40 @@
-const getCommerceOwnerInfo = (req, res) => {
-  const CommerceOwnerId = req.params.id;
+const databaseInstance = require("../db");
 
 
-res.send(`this is the information of this merchant ${CommerceOwnerId}`)
+
+const getCommerceOwnerInfo = async (req, res) => {
+  const commerceOwnerId = req.params.id;
+  let formatedCommerceInfo = ''
+
+
+  const rawCommerceInfo = await databaseInstance.query(
+    "select * from comercios where comerciante_id_comerciante =" +commerceOwnerId + ";"
+  )
+
+
+  try {
+    formatedCommerceInfo = LookIfCommerceExists(rawCommerceInfo);
+  } catch (error) {
+    formatedCommerceInfo = 'Este comercio no existe'
+  }
+
+
+res.send(`this is the information of this merchant ` + formatedCommerceInfo)
+}
+
+
+
+function LookIfCommerceExists(rawCommerceInfo) {
+  const commerceDoesntExist = 0
+
+  
+  if(rawCommerceInfo.rowCount === commerceDoesntExist ) {
+
+    throw Error('This commerce Doesnt exist')
+
+  }else {
+    return rawCommerceInfo.rows
+  }
 }
 
 module.exports = {
