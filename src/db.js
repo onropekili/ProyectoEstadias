5aa5d0621a4044e145c06587bd9d008a54efeb40
Yahir <cristopher.alvarado.21s@utzmg.edu.mx>
2023-06-15 17:52:26 -0600

    Modified envairoment variables

--- src/db.js
+++ src/db.js
@@ -1,8 +1,9 @@
 const { Pool } = require("pg");
+import dotenv, { configDotenv } from 'dotenv';
 
 const pool = new Pool({
   user: "postgres",
-  password: "123456",
+  password: process.env.DATABASE_PASSWORD,
   host: "localhost",
   port: 5432,
   database: "ProyectoEstadias",
