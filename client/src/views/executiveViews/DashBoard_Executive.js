c97953d07d356b2dec4a2669fe58c126a02be652
Angelica <angelica.silva.21s@utzmg.edu.mx>
2023-08-28 18:29:48 -0600

    Made Responsive dashboard Admin

--- client/src/views/executiveViews/DashBoard_Executive.js
+++ client/src/views/executiveViews/DashBoard_Executive.js
@@ -138,110 +138,109 @@ function DashBoard_Ejecutivo() {
   return (
     <>
       <Header useButton={true} currentPage={DashBoard_Ejecutivo} />
-      <div className="flex px-8 pt-7">
-        <div className="flex flex-col">
-          <div className="mb-4 flex flex-wrap">
-            <div className="relative flex items-center mr-4">
-              <input
-                value={nameOrId}
-                id="nameOrId"
-                type="text"
-                className="pl-14 px-4 py-2.5 w-96 max-w-lg rounded-lg font-Foco-Corp text-gris placeholder:text-base placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none sm:text-sm sm:leading-6"
-                placeholder="Nombre O No. de folio"
-                onChange={HandleInputChanges}
-              />
-              <img
-                src={require("../../assets/images/search.png")}
-                alt="Img"
-                className="absolute left-0 w-11 h-11 p-2 border border-gris rounded-lg rounded-r-none"
-              />
-            </div>
-            <select
-              value={Mostrar}
-              id="mostrar"
-              className=" px-4 py-3.5 w-64 rounded-lg font-Foco-Corp text-gris placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none sm:text-sm sm:leading-6"
-              onChange={HandleInputChanges}
-            >
-              <option value="">Todos</option>
-              <option value="permanentes">Permanentes</option>
-              <option value="eventuales">Eventuales</option>
-            </select>
-          </div>
-
-          <div className="mb-4 flex flex-wrap">
-            <div className="relative flex items-center mr-4">
-              <input
-                value={colonia}
-                id="colonia"
-                type="text"
-                className="pl-14 px-4 py-2.5 w-96 max-w-lg rounded-lg font-Foco-Corp text-gris placeholder:text-base placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none sm:text-sm sm:leading-6"
-                placeholder="Buscar por colonia"
-                onInput={HandleInputChanges}
-              />
-              <img
-                src={require("../../assets/images/search.png")}
-                alt="Img"
-                className="absolute left-0 w-11 h-11 p-2 border border-gris rounded-lg rounded-r-none"
-              />
-            </div>
-            <select
-              id="filtrar"
-              value={filtrarPor}
-              name="filtrar"
-              className="px-4 py-3.5 w-64 rounded-lg font-Foco-Corp text-gris placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none sm:text-sm sm:leading-6"
-              onInput={HandleInputChanges}
-            >
-              <option  value="">
-                Filtrar por
-              </option>
-              <option value="terceraEdad">
-                Tercera edad/Capacidades diferentes
-              </option>
-              <option value="refrendados">Refrendados</option>
-              <option value="noRefrendados">No Refrendados</option>
-              <option value="conObservacion">Con Observaciones</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="flex flex-col ml-auto justify-end">
-          <div className="flex mb-auto">
-            <div className="mb-4 flex flex-wrap">
-              <div className="relative flex items-center mr-4">
-                <button
-                  onClick={() => navigate("/Registrar-comerciante")}
-                  className="bg-naranja hover:bg-naranja hover:opacity-75 text-white font-bold py-2 px-20 rounded"
-                >
-                  Alta vía pública
-                </button>
-                <img
-                  src={require("../../assets/images/edit.png")}
-                  alt="Img"
-                  className="absolute left-0 w-11 h-11 p-2 border-2 border-white rounded-md rounded-r-none"
-                />
-              </div>
-            </div>
-            <div className="mb-4 flex flex-wrap">
+      <div className="px-8 pt-7">
+        <div className="flex flex-col lg:grid lg:grid-cols-4 items-start gap-4">
+          <div className="col-span-2 flex flex-col lg:grid lg grid-cols-2 gap-4">
+            <div className=" flex flex-col gap-2 w-full">
               <div className="relative flex items-center">
-                <button
-                  onClick={() => navigate("/Registrar-comerciante-evento")}
-                  className="bg-verde hover:bg-verde hover:opacity-75 text-white font-bold py-2 px-20 rounded"
-                >
-                  Alta por evento
-                </button>
-                <img
-                  src={require("../../assets/images/edit.png")}
-                  alt="Img"
-                  className="absolute left-0 w-11 h-11 p-2 border-2 border-white rounded-md rounded-r-none"
+                <input
+                  value={nameOrId}
+                  id="nameOrId"
+                  type="text"
+                  className="pl-14 px-4 py-2.5 w-96 max-w-lg rounded-lg font-Foco-Corp text-gris placeholder:text-base placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none sm:text-sm sm:leading-6"
+                  placeholder="Nombre O No. de folio"
+                  onChange={HandleInputChanges}
                 />
+                <img
+                  src={require("../../assets/images/search.png")}
+                  alt="Img"
+                  className="absolute left-0 w-11 h-11 p-2 border border-gris rounded-lg rounded-r-none"
+                />
+              </div>
+              <div className="relative flex items-center">
+                <input
+                  value={colonia}
+                  id="colonia"
+                  type="text"
+                  className="pl-14 px-4 py-2.5 w-96 max-w-lg rounded-lg font-Foco-Corp text-gris placeholder:text-base placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none sm:text-sm sm:leading-6"
+                  placeholder="Buscar por colonia"
+                  onInput={HandleInputChanges}
+                />
+                <img
+                  src={require("../../assets/images/search.png")}
+                  alt="Img"
+                  className="absolute left-0 w-11 h-11 p-2 border border-gris rounded-lg rounded-r-none"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 w-full">
+              <select
+                id="filtrar"
+                value={filtrarPor}
+                name="filtrar"
+                className="px-4 py-3.5 w-64 h-11 rounded-lg font-Foco-Corp text-gris placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none sm:text-sm sm:leading-6"
+                onInput={HandleInputChanges}
+              >
+                <option  value="">
+                  Filtrar por
+                </option>
+                <option value="terceraEdad">
+                  Tercera edad/Capacidades diferentes
+                </option>
+                <option value="refrendados">Refrendados</option>
+                <option value="noRefrendados">No Refrendados</option>
+                <option value="conObservacion">Con Observaciones</option>
+              </select>
+              <select
+                value={Mostrar}
+                id="mostrar"
+                className=" px-4 py-3.5 w-64 h-11 rounded-lg font-Foco-Corp text-gris placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none sm:text-sm sm:leading-6"
+                onChange={HandleInputChanges}
+              >
+                <option value="">Todos</option>
+                <option value="permanentes">Permanentes</option>
+                <option value="eventuales">Eventuales</option>
+              </select>
+            </div>
+          </div>
+          <div className="col-span-2 flex flex-col w-full">
+            <div className="flex flex-col lg:grid lg:grid-cols-2 gap-4">
+              <div className="flex">
+                <div className="relative flex items-center w-full">
+                  <button
+                    onClick={() => navigate("/Registrar-comerciante")}
+                    className="bg-naranja hover:bg-naranja hover:opacity-75 text-white font-bold w-full py-2 px-20 rounded"
+                  >
+                    Alta vía pública
+                  </button>
+                  <img
+                    src={require("../../assets/images/edit.png")}
+                    alt="Img"
+                    className="absolute left-0 w-11 h-11 p-2 border-2 border-white rounded-md rounded-r-none"
+                  />
+                </div>
+              </div>
+              <div className="flex">
+                <div className="relative flex items-center w-full">
+                  <button
+                    onClick={() => navigate("/Registrar-comerciante-evento")}
+                    className="bg-verde hover:bg-verde hover:opacity-75 text-white font-bold w-full py-2 px-20 rounded"
+                  >
+                    Alta por evento
+                  </button>
+                  <img
+                    src={require("../../assets/images/edit.png")}
+                    alt="Img"
+                    className="absolute left-0 w-11 h-11 p-2 border-2 border-white rounded-md rounded-r-none"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="justify-center items-center flex flex-wrap">
-        {ComerciantesComponents}
+        <div className="justify-center items-center flex flex-wrap gap-5 py-5">
+          {ComerciantesComponents}
+        </div>
       </div>
     </>
   );
