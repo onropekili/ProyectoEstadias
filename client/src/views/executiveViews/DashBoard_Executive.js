9d3d196eeac1d4278905e1c6da34c648eb1df200
Angelica <angelica.silva.21s@utzmg.edu.mx>
2023-08-30 19:14:00 -0600

    Made Select Styles

--- client/src/views/executiveViews/DashBoard_Executive.js
+++ client/src/views/executiveViews/DashBoard_Executive.js
@@ -4,6 +4,7 @@ import { AuthMiddleware } from "../../middleware/ProtectedMiddleware";
 import { useLocation, useNavigate } from "react-router-dom";
 import { InfoComponent } from "../../components/InfoComponent";
 import Header from "../../components/Header";
+import { AiFillCaretDown } from 'react-icons/ai';
 
 function DashBoard_Ejecutivo() {
   const location = useLocation();
@@ -147,14 +148,14 @@ function DashBoard_Ejecutivo() {
                   value={nameOrId}
                   id="nameOrId"
                   type="text"
-                  className="w-full h-11 pl-14 px-4 py-2.5 text-lg rounded-lg font-Foco-Corp text-gris placeholder:text-lg placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none"
+                  className="w-full h-11 pl-14 px-4 text-lg rounded-lg font-Foco-Corp text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none"
                   placeholder="Nombre O No. de folio"
                   onChange={HandleInputChanges}
                 />
                 <img
                   src={require("../../assets/images/search.png")}
                   alt="Img"
-                  className="absolute left-0 w-11 h-11 p-2 border-r-2 border-gris rounded-lg rounded-r-none"
+                  className="absolute left-0 w-11 h-11 p-2 border-r-2 border-gris bg-gris bg-opacity-5"
                 />
               </div>
               <div className="relative flex items-center w-full">
@@ -162,41 +163,51 @@ function DashBoard_Ejecutivo() {
                   value={colonia}
                   id="colonia"
                   type="text"
-                  className="w-full h-11 pl-14 px-4 py-2.5 text-lg rounded-lg font-Foco-Corp text-gris placeholder:text-lg placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none"
+                  className="w-full h-11 pl-14 px-4 text-lg rounded-lg font-Foco-Corp text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none"
                   placeholder="Buscar por colonia"
                   onInput={HandleInputChanges}
                 />
                 <img
                   src={require("../../assets/images/search.png")}
                   alt="Img"
-                  className="absolute left-0 w-11 h-11 p-2 border-r-2 border-gris rounded-lg rounded-r-none"
+                  className="absolute left-0 w-11 h-11 p-2 border-r-2 border-gris bg-gris bg-opacity-5"
                 />
               </div>
             </div>
             <div className="flex flex-col gap-3 w-full">
-              <select
-                id="filtrar"
-                value={filtrarPor}
-                name="filtrar"
-                className="w-full h-11 px-4 text-lg rounded-lg font-Foco-Corp text-gris placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none"
-                onInput={HandleInputChanges}
-              >
-                <option  value="">Filtrar por</option>
-                <option value="terceraEdad">Tercera edad/Capacidades diferentes</option>
-                <option value="refrendados">Refrendados</option>
-                <option value="noRefrendados">No Refrendados</option>
-                <option value="conObservacion">Con Observaciones</option>
-              </select>
-              <select
-                value={Mostrar}
-                id="mostrar"
-                className="w-full h-11 px-4 text-lg rounded-lg font-Foco-Corp text-gris placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none"
-                onChange={HandleInputChanges}
-              >
-                <option value="">Todos</option>
-                <option value="permanentes">Permanentes</option>
-                <option value="eventuales">Eventuales</option>
-              </select>
+              <div className="relative flex items-center w-full">
+                <select
+                  id="filtrar"
+                  value={filtrarPor}
+                  name="filtrar"
+                  className="w-full h-11 px-4 text-lg rounded-lg font-Foco-Corp text-gris placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none appearance-none"
+                  onInput={HandleInputChanges}
+                >
+                  <option value="">Filtrar por</option>
+                  <option value="terceraEdad">Tercera edad/Capacidades diferentes</option>
+                  <option value="refrendados">Refrendados</option>
+                  <option value="noRefrendados">No Refrendados</option>
+                  <option value="conObservacion">Con Observaciones</option>
+                </select>
+                <div className="absolute right-0 top-1/2 transform -translate-y-1/2 pointer-events-none border-l-2 border-gris px-2 py-2 bg-gris bg-opacity-5">
+                  <AiFillCaretDown size={26} color="#9CA3AF" /> {/* Icono de flecha */}
+                </div>
+              </div>
+              <div className="relative flex items-center w-full">
+                <select
+                  value={Mostrar}
+                  id="mostrar"
+                  className="w-full h-11 px-4 text-lg rounded-lg font-Foco-Corp text-gris placeholder:text-gris placeholder:text-opacity-70 shadow-sm ring-2 ring-inset ring-gris focus:drop-shadow-lg focus:ring-2 focus:ring-gris focus:ring-opacity-75 focus:outline-none appearance-none bg-transparent"
+                  onChange={HandleInputChanges}
+                >
+                  <option value="">Todos</option>
+                  <option value="permanentes">Permanentes</option>
+                  <option value="eventuales">Eventuales</option>
+                </select>
+                <div className="absolute right-0 top-1/2 transform -translate-y-1/2 pointer-events-none border-l-2 border-gris px-2 py-2 bg-gris bg-opacity-5">
+                  <AiFillCaretDown size={26} color="#9CA3AF" /> {/* Icono de flecha */}
+                </div>
+              </div>
             </div>
           </div>
           <div className="col-span-2 flex flex-col w-full">
